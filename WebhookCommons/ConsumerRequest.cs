af63d77fbac6e0ac9cbace99da53046bcccfe4b6
Can Acar <can.acar2@ogr.sakarya.edu.tr>
2022-03-09 00:28:25 +0300

    Update ConsumerRequest.cs

--- WebhookCommons/ConsumerRequest.cs
+++ WebhookCommons/ConsumerRequest.cs
@@ -3,9 +3,17 @@ namespace WebhookCommons;
 public class ConsumerRequest
 {
     
+    
 }
 
 public class ConsumerResponse
 {
     
+}
+
+public interface IWebhookBroker:IDisposable
+{
+   void Publish<T>(object source, T message) where T : new ();
+   void Subscribe<T>(object source, Action<T> subscription) where T : new ();
+   void Unsubscribe<T>( Action<T> subscription) where T : new ();
 }
